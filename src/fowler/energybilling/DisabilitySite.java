a5a2cc2791df015a36e844f58cfaf8ffebf49416
Sascha Grossenbacher <saschagros@gmail.com>
2011-03-31 17:50:12 +0200

    Verwenden der Calendar-Klasse zum Berechnen von "dayOfYear"

--- src/fowler/energybilling/DisabilitySite.java
+++ src/fowler/energybilling/DisabilitySite.java
@@ -1,5 +1,6 @@
 package fowler.energybilling;
 
+import java.util.Calendar;
 import java.util.Date;
 
 public class DisabilitySite {
@@ -69,54 +70,9 @@ public class DisabilitySite {
 	}
 
 	int dayOfYear(Date arg) {
-		int result;
-		switch (arg.getMonth()) {
-		case 0:
-			result = 0;
-			break;
-		case 1:
-			result = 31;
-			break;
-		case 2:
-			result = 59;
-			break;
-		case 3:
-			result = 90;
-			break;
-		case 4:
-			result = 120;
-			break;
-		case 5:
-			result = 151;
-			break;
-		case 6:
-			result = 181;
-			break;
-		case 7:
-			result = 212;
-			break;
-		case 8:
-			result = 243;
-			break;
-		case 9:
-			result = 273;
-			break;
-		case 10:
-			result = 304;
-			break;
-		case 11:
-			result = 334;
-			break;
-		default:
-			throw new IllegalArgumentException();
-		}
-		result += arg.getDate();
-		// check leap year
-		if ((arg.getYear() % 4 == 0)
-				&& ((arg.getYear() % 100 != 0) || ((arg.getYear() + 1900) % 400 == 0))) {
-			result++;
-		}
-		return result;
+		Calendar c = Calendar.getInstance();
+		c.setTime(arg);
+		return c.get(Calendar.DAY_OF_YEAR);
 	}
 
 }
