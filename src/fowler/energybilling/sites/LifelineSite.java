7692fea649ef2b7d0da7d3e764948e1f11cc82bb
Sascha Grossenbacher <saschagros@gmail.com>
2011-04-14 15:22:54 +0200

    Extract getBaseUsage()

--- src/fowler/energybilling/sites/LifelineSite.java
+++ src/fowler/energybilling/sites/LifelineSite.java
@@ -7,14 +7,7 @@ public class LifelineSite extends GenericSite {
 
 	@Override
 	protected Dollars charge (int usage) {
- 		double base = Math.min(usage,100) * 0.03;
-		if (usage > 100) {
-			base += (Math.min (usage,200) - 100) * 0.05;
-		}
-		if (usage > 200) {
-			base += (usage - 200) * 0.07;
-		}
-		Dollars result = new Dollars(base);
+ 		Dollars result = getBaseUsage(usage);
 		result = result.minus(new Dollars(8)).max(new Dollars(0));
 		Dollars tax = result.times(TAX_RATE);
 		result.plus(tax);
@@ -22,5 +15,16 @@ public class LifelineSite extends GenericSite {
 		result.plus(fuelCharge.times(1 + TAX_RATE));
 		result.round(2);
 		return result;
+	}
+
+	private Dollars getBaseUsage(int usage) {
+		double base = Math.min(usage,100) * 0.03;
+		if (usage > 100) {
+			base += (Math.min (usage,200) - 100) * 0.05;
+		}
+		if (usage > 200) {
+			base += (usage - 200) * 0.07;
+		}
+		return new Dollars(base);
 	}
 }
