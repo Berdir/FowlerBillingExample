2a4d2af5716c619be10e136a23ba66f167118c98
Sascha Grossenbacher <saschagros@gmail.com>
2011-04-14 15:04:31 +0200

    working on simplifying charge() of DisabilitySite

--- src/fowler/energybilling/sites/DisabilitySite.java
+++ src/fowler/energybilling/sites/DisabilitySite.java
@@ -9,6 +9,7 @@ public class DisabilitySite extends TimedSite {
 	private static final Dollars FUEL_TAX_CAP = new Dollars(0.10);
 	private static final double TAX_RATE = 0.05;
 	private static final int CAP = 200;
+	private static final double FUEL = 0.0175;
 
 	public DisabilitySite(Zone zone) {
 		super(zone);
@@ -16,22 +17,18 @@ public class DisabilitySite extends TimedSite {
 
 	@Override
 	protected Dollars charge(int fullUsage, Date start, Date end) {
-		Dollars result;
-		
 		// JK: don't charge more than the CAP (which is set to 200)
 		int usage = Math.min(fullUsage, CAP);
 		
-		result = calculateSummerWinterRate(usage, start, end);
+		Dollars result = calculateSummerWinterRate(usage, start, end);
 		// if the full usage is below the cap the max yields zero
-		result = result
-				.plus(new Dollars(Math.max(fullUsage - usage, 0) * 0.062));
+		result.plus(new Dollars(Math.max(fullUsage - usage, 0) * 0.062));
 
-		result = result.plus(new Dollars(result.times(TAX_RATE)));
-		Dollars fuel = new Dollars(fullUsage * 0.0175);
-		result = result.plus(fuel);
-		result = new Dollars(result.plus(fuel.times(TAX_RATE).min(FUEL_TAX_CAP)));
-		result = result.round(2);
+		result.plus(result.times(TAX_RATE));
+		Dollars fuel = new Dollars(fullUsage * FUEL);
+		result.plus(fuel);
+		result.plus(fuel.times(TAX_RATE).min(FUEL_TAX_CAP));
+		result.round(2);
 		return result;
 	}
-
 }
